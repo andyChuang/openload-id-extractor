3a4c890f3c099b9815b201dd8219e30545d1c9ea
AndyChuang <andy.chuang@mirlab.org>
2016-07-08 16:48:18 +0800

    Prepare to release.

--- js/popup.js
+++ js/popup.js
@@ -54,7 +54,7 @@ function onWindowLoad() {
   message = document.querySelector('#message');
 
   chrome.tabs.executeScript(null, {
-    file: "getPagesSource.js"
+    file: "js/getPagesSource.js"
   }, function() {
     // If you try and inject into an extensions page or the webstore/NTP you'll get an error
     if (chrome.runtime.lastError) {
