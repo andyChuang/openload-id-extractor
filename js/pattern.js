c3233349ab078b3dfc0f36f978efdf36218340ba
AndyChuang <andychuang@cybersoft4u.com>
2017-05-14 21:01:22 +0800

    Update some url patterns

--- js/pattern.js
+++ js/pattern.js
@@ -1,5 +1,7 @@
 var openloadPattern = [
   /\"https?:\/\/openload\.co\/embed\/(.*?)\/.*?\"/gi,
   /\"https?:\/\/openload\.co\/embed\/(.*?)\"/gi,
-  /\"https?:\/\/openload\.co\/f\/(.*?)\/\"/gi
+  /\"https?:\/\/openload\.co\/f\/(.*?)\/\"/gi,
+  /\"https?:\/\/openload\.co\/f\/(.*?)\/.*?\"/gi,
+  /\"https?:\/\/thumb\.openload\.co\/splash\/(.*?)\/.*?\"/gi
   ];
